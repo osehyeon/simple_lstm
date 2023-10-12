c33f2a30610ee940ecc83ff2054866833a52cd20
somae <somae604@naver.com>
2023-10-12 14:11:20 +0900

    add file

--- lstm/lstm-onnx-module-10-hls.c
+++ lstm/lstm-onnx-module-10-hls.c
@@ -19,19 +19,67 @@ TensorY cell_state(Gate ft, Gate it, Gate ct, TensorY Y_c);
 TensorY output(Gate ot, TensorY Y_c, TensorY Y_h);
 
 TensorY entry(float tensor_X[10][1][1],
-            float Wi[1][10][1], float Wo[1][10][1], float Wf[1][10][1], float Wu[1][10][1],
-            float Ri[1][10][10], float Ro[1][10][10], float Rf[1][10][10], float Ru[1][10][10],
-            float Bi[1][10], float Bo[1][10], float Bf[1][10], float Bu[1][10],
-            float temp_Bi[1][10], float temp_Bo[1][10], float temp_Bf[1][10], float temp_Bu[1][10]) {
-
-
+            float tensor_W[1][40][1],
+            float tensor_R[1][40][10],
+            float tensor_B[1][80]) {
+                
     TensorY Y_h = {{{0}}};
     TensorY Y_c = {{{0}}};
     Gate it = {{0}};
     Gate ot = {{0}};
     Gate ft = {{0}};
     Gate ct = {{0}};
+    
+    int hs = 10;
+    
     float X[1][1][1]= {{{0}}};
+    float Wi[1][10][1] = {{{0}}};
+    float Wo[1][10][1] = {{{0}}};
+    float Wf[1][10][1] = {{{0}}};
+    float Wu[1][10][1] = {{{0}}};
+    float Ri[1][10][10] = {{{0}}};
+    float Ro[1][10][10] = {{{0}}};
+    float Rf[1][10][10] = {{{0}}};
+    float Ru[1][10][10] = {{{0}}};
+    float Bi[1][10] = {{0}};
+    float Bo[1][10] = {{0}};
+    float Bf[1][10] = {{0}};
+    float Bu[1][10] = {{0}};
+    float temp_Bi[1][10] = {{0}};
+    float temp_Bo[1][10] = {{0}};
+    float temp_Bf[1][10] = {{0}};
+    float temp_Bu[1][10] = {{0}};
+
+    for(int i=0; i<hs; i++)
+    {
+        Wi[0][i][0] = tensor_W[0][i][0];
+        Wo[0][i][0] = tensor_W[0][hs + i][0];
+        Wf[0][i][0] = tensor_W[0][2*hs + i][0];
+        Wu[0][i][0] = tensor_W[0][3*hs + i][0];
+    }
+
+    for(int i=0; i<hs; i++)
+    {
+        for(int j=0; j<hs; j++)
+        {
+            Ri[0][i][j] = tensor_R[0][i][j];
+            Ro[0][i][j] = tensor_R[0][hs + i][j];
+            Rf[0][i][j] = tensor_R[0][2*hs + i][j];
+            Ru[0][i][j] = tensor_R[0][3*hs + i][j];
+        }
+    }
+
+    for(int i=0; i<hs; i++)
+    {
+        Bi[0][i] = tensor_B[0][i];
+        Bo[0][i] = tensor_B[0][hs + i];
+        Bf[0][i] = tensor_B[0][2*hs + i];
+        Bu[0][i] = tensor_B[0][3*hs + i];
+        temp_Bi[0][i] = tensor_B[0][4*hs + i];
+        temp_Bo[0][i] = tensor_B[0][5*hs + i];
+        temp_Bf[0][i] = tensor_B[0][6*hs + i];
+        temp_Bu[0][i] = tensor_B[0][7*hs + i];
+    }
 
     for(int i = 0; i < 10; i++)
     {
@@ -47,14 +95,12 @@ TensorY entry(float tensor_X[10][1][1],
         ot = output_gate(X, Wo, Ro, Bo, Y_h);
         ft = forget_gate(X, Wf, Rf, Bf, Y_h);
         ct = update_gate(X, Wu, Ru, Bu, Y_h);
-        
+
         Y_c = cell_state(ft, it, ct, Y_h);
         Y_h = output(ot, Y_h, Y_c);
 
     }
-
     return Y_h;
-    
 }
 
 float sigmoid(float input) {
