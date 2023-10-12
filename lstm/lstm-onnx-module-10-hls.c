ad006b93caa6487f1c994455b4323bc746d1121f
somae604 <somae604@naver.com>
2023-10-12 15:28:35 +0900

    add file

--- lstm/lstm-onnx-module-10-hls.c
+++ lstm/lstm-onnx-module-10-hls.c
@@ -1,5 +1,6 @@
 #include <math.h>
 #include <string.h>
+#include <stdio.h>
 
 typedef struct { 
     float data[1][10];
@@ -19,9 +20,10 @@ TensorY cell_state(Gate ft, Gate it, Gate ct, TensorY Y_c);
 TensorY output(Gate ot, TensorY Y_c, TensorY Y_h);
 
 TensorY entry(float tensor_X[10][1][1],
-            float tensor_W[1][40][1],
-            float tensor_R[1][40][10],
-            float tensor_B[1][80]) {
+            float Wi[1][10][1], float Wo[1][10][1], float Wf[1][10][1], float Wu[1][10][1],
+            float Ri[1][10][10], float Ro[1][10][10], float Rf[1][10][10], float Ru[1][10][10],
+            float Bi[1][10], float Bo[1][10], float Bf[1][10], float Bu[1][10],
+            float temp_Bi[1][10], float temp_Bo[1][10], float temp_Bf[1][10], float temp_Bu[1][10]) {
                 
     TensorY Y_h = {{{0}}};
     TensorY Y_c = {{{0}}};
@@ -30,56 +32,7 @@ TensorY entry(float tensor_X[10][1][1],
     Gate ft = {{0}};
     Gate ct = {{0}};
     
-    int hs = 10;
-    
     float X[1][1][1]= {{{0}}};
-    float Wi[1][10][1] = {{{0}}};
-    float Wo[1][10][1] = {{{0}}};
-    float Wf[1][10][1] = {{{0}}};
-    float Wu[1][10][1] = {{{0}}};
-    float Ri[1][10][10] = {{{0}}};
-    float Ro[1][10][10] = {{{0}}};
-    float Rf[1][10][10] = {{{0}}};
-    float Ru[1][10][10] = {{{0}}};
-    float Bi[1][10] = {{0}};
-    float Bo[1][10] = {{0}};
-    float Bf[1][10] = {{0}};
-    float Bu[1][10] = {{0}};
-    float temp_Bi[1][10] = {{0}};
-    float temp_Bo[1][10] = {{0}};
-    float temp_Bf[1][10] = {{0}};
-    float temp_Bu[1][10] = {{0}};
-
-    for(int i=0; i<hs; i++)
-    {
-        Wi[0][i][0] = tensor_W[0][i][0];
-        Wo[0][i][0] = tensor_W[0][hs + i][0];
-        Wf[0][i][0] = tensor_W[0][2*hs + i][0];
-        Wu[0][i][0] = tensor_W[0][3*hs + i][0];
-    }
-
-    for(int i=0; i<hs; i++)
-    {
-        for(int j=0; j<hs; j++)
-        {
-            Ri[0][i][j] = tensor_R[0][i][j];
-            Ro[0][i][j] = tensor_R[0][hs + i][j];
-            Rf[0][i][j] = tensor_R[0][2*hs + i][j];
-            Ru[0][i][j] = tensor_R[0][3*hs + i][j];
-        }
-    }
-
-    for(int i=0; i<hs; i++)
-    {
-        Bi[0][i] = tensor_B[0][i];
-        Bo[0][i] = tensor_B[0][hs + i];
-        Bf[0][i] = tensor_B[0][2*hs + i];
-        Bu[0][i] = tensor_B[0][3*hs + i];
-        temp_Bi[0][i] = tensor_B[0][4*hs + i];
-        temp_Bo[0][i] = tensor_B[0][5*hs + i];
-        temp_Bf[0][i] = tensor_B[0][6*hs + i];
-        temp_Bu[0][i] = tensor_B[0][7*hs + i];
-    }
 
     for(int i = 0; i < 10; i++)
     {
@@ -95,11 +48,10 @@ TensorY entry(float tensor_X[10][1][1],
         ot = output_gate(X, Wo, Ro, Bo, Y_h);
         ft = forget_gate(X, Wf, Rf, Bf, Y_h);
         ct = update_gate(X, Wu, Ru, Bu, Y_h);
-
-        Y_c = cell_state(ft, it, ct, Y_h);
-        Y_h = output(ot, Y_h, Y_c);
-
+        Y_c = cell_state(ft, it, ct, Y_c);
+        Y_h = output(ot, Y_c, Y_h);
     }
+
     return Y_h;
 }
 
@@ -163,21 +115,20 @@ Gate forget_gate(float X[1][1][1], float W[1][10][1], float R[1][10][10], float 
     const int ds = 1;
     const int bs = 1;
 
-    Gate gate = {{{0}}};  
+    Gate ft = {{{0}}};  
 
     for(int b=0; b<bs; b++)
     for(int h=0; h<hs; h++) {
         for(int i=0; i<ds; i++) {
-            gate.data[b][h] += X[0][b][i]*W[0][h][i];
+            ft.data[b][h] += X[0][b][i]*W[0][h][i];
         }
         for(int k=0; k<hs; k++) {
-            gate.data[b][h] += Y_h.data[0][b][k]*R[0][h][k];
+            ft.data[b][h] += Y_h.data[0][b][k]*R[0][h][k];
         }
-        gate.data[b][h] += B[0][h];
-        gate.data[b][h] = sigmoid(gate.data[b][h]);
+        ft.data[b][h] += B[0][h];
+        ft.data[b][h] = sigmoid(ft.data[b][h]);
     }
-
-    return gate;
+    return ft;
 }
 
 
@@ -187,22 +138,22 @@ Gate update_gate(float X[1][1][1], float W[1][10][1], float R[1][10][10], float 
     const int ds = 1;
     const int bs = 1;
 
-    Gate gate = {{{0}}}; 
+    Gate ct = {{{0}}}; 
 
     for(int b=0; b<bs; b++)
     for(int h=0; h<hs; h++) {
-        gate.data[b][h]= 0;
+        ct.data[b][h]= 0;
         for(int i=0; i<ds; i++) {
-            gate.data[b][h] += X[0][b][i]*W[0][h][i];
+            ct.data[b][h] += X[0][b][i]*W[0][h][i];
         }
         for(int k=0; k<hs; k++) {
-            gate.data[b][h] += Y_h.data[0][b][k]*R[0][h][k];
+            ct.data[b][h] += Y_h.data[0][b][k]*R[0][h][k];
         }
-        gate.data[b][h] += B[0][h];
-        gate.data[b][h] = tan_h(gate.data[b][h]);
+        ct.data[b][h] += B[0][h];
+        ct.data[b][h] = tan_h(ct.data[b][h]);
     }
 
-    return gate; 
+    return ct; 
 }
 
 TensorY cell_state(Gate ft, Gate it, Gate ct, TensorY Y_c) {
@@ -214,7 +165,6 @@ TensorY cell_state(Gate ft, Gate it, Gate ct, TensorY Y_c) {
     for( int h=0; h<hs; h++) {
         Y_c.data[0][b][h] = Y_c.data[0][b][h]*ft.data[b][h] + it.data[b][h]*ct.data[b][h];
     }
-
     return Y_c;
 }
 
@@ -226,7 +176,7 @@ TensorY output(Gate ot, TensorY Y_c, TensorY Y_h) {
     for( int b=0; b<bs; b++)
       for( int h=0; h<hs; h++) {
          Y_h.data[0][b][h] = ot.data[b][h] * tan_h(Y_c.data[0][b][h]);
-      }
 
+      }
       return Y_h;
 }
--- lstm/lstm-pytorch-inference.py
+++ lstm/lstm-pytorch-inference.py
@@ -5,7 +5,7 @@ import onnxruntime as ort
 PI = 3.14159265358979323846
 
 # 1. onnxruntime 세션 생성하기
-ort_session = ort.InferenceSession("lstm-pytorch-10-1.onnx")
+ort_session = ort.InferenceSession("lstm-pytorch-10.onnx")
 
 # 2. 예측 및 검증을 위한 데이터 생성하기
 def generate_data():
@@ -19,7 +19,7 @@ def generate_data():
 
 inputs = generate_data()
 
-print(inputs)
+#print(inputs)
 # ONNX 모델을 사용하여 예측하기
 ort_inputs = {ort_session.get_inputs()[0].name: inputs}
 ort_outs = ort_session.run(None, ort_inputs)
